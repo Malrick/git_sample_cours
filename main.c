20515f0ca84817166ad51982fe018dae6113971a
Alexhuang03 <alexhuang392@yahoo.com>
2023-04-21 16:00:08 +0200

    commit par alex

--- main.c
+++ main.c
@@ -3,6 +3,6 @@
 int main() {
     printf("Hello, Malrick! Ceci est un troisième commit, modifié par Elias\n");
     printf("\ncoucou c'est chloé");
-    printf("Hello, Malrick! Ceci est le je sais pas combientème commit, modifié par Alex \n");
+    printf("Hello, Malrick! Ceci est le je sais pas combientème commit, modifié par Alex HUANG \n");
     return 0;
 }
